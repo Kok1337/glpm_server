58fde8329359784704e37e07f845f9b2d68f515a
Aleksey Bondarenko <aleksey.bondarenko.2001@mail.ru>
2022-09-28 13:53:35 +0700

    Создание проекта

---
+++ src/main/kotlin/com/kok1337/glpm/GlpmApplication.kt
@@ -0,0 +1,11 @@
+package com.kok1337.glpm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GlpmApplication
+
+fun main(args: Array<String>) {
+    runApplication<GlpmApplication>(*args)
+}
---
+++ src/test/kotlin/com/kok1337/glpm/GlpmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kok1337.glpm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GlpmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
